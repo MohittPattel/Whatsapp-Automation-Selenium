e2a10c72a83c5551516854eb42c52a53699de350
Mohit Patel <76245882+MohittPattel@users.noreply.github.com>
2021-08-31 21:03:11 +0530

    Update Main.py
    code is added

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
+from selenium import webdriver
 
+driver = webdriver.Chrome()
+
+driver.get("https://web.whatspp.com/")
+
+input("Scam QR code and press any key to start")
+
+var=driver.find_element_by_css_selector('span["title=Rohit Patel"]')
+var.click()
+
+testinput = driver.find_element_by_xpath("/html/body/div[1]/div/div[4]")
+
+for i in range(10)
+testinput.send_keys("Selenium Testing")
+testinput.send_Keys(keys.RETURN)
